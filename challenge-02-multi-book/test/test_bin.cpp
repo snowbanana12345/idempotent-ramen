c04f689ae692369a36869965ee3962ad3cec7351
zhangzhongming <icebullet12345@gmail.com>
2026-07-27 20:06:46 +0800

    add more unittests

--- challenge-02-multi-book/test/test_bin.cpp
+++ challenge-02-multi-book/test/test_bin.cpp
@@ -206,13 +206,6 @@ TEST(Modify, PriceChange){
     EXPECT_EQ(ask.price, price_2);
     EXPECT_EQ(ask.qty, qty_2);
 
-    QueuePosition q1 = book.get_queue_position(our_id_1);
-    EXPECT_EQ(q1.index, 1);
-    EXPECT_EQ(q1.qty_ahead, qty_2);
-    QueuePosition q2 = book.get_queue_position(our_id_2);
-    EXPECT_EQ(q2.index, 0);
-    EXPECT_EQ(q2.qty_ahead, 0);
-
     EXPECT_CALL(venue, modify_order(_,_,_)).WillOnce(Return(exch_id_3));
     book.modify_our_order(our_id_1, price_3, qty_1);
     book.cancel_order(exch_id_1);
@@ -221,14 +214,6 @@ TEST(Modify, PriceChange){
     ask = book.best_ask(symbol);
     EXPECT_EQ(ask.price, price_3);
     EXPECT_EQ(ask.qty, qty_1);
-
-    // the queue position swaps over since the increase in qty forces order1 to give up its queue position
-    q1 = book.get_queue_position(our_id_1);
-    EXPECT_EQ(q1.index, 0);
-    EXPECT_EQ(q1.qty_ahead, 0);
-    q2 = book.get_queue_position(our_id_2);
-    EXPECT_EQ(q2.index, 1);
-    EXPECT_EQ(q2.qty_ahead, qty_1);
 }
 
 TEST(Query, AskSorted){
@@ -246,24 +231,231 @@ TEST(Query, AskSorted){
     book.send_order(our_id_2, symbol, side, 98, 7);
     book.add_order(exch_id_2, symbol, side, 98, 7);
     EXPECT_CALL(venue, send_order(_,_,_,_,_)).WillOnce(Return(exch_id_3));
-    book.send_order(our_id_3, symbol, side, 99, 11);
-    book.add_order(exch_id_3, symbol, side, 99, 11);
+    book.send_order(our_id_3, symbol, side, 100, 11);
+    book.add_order(exch_id_3, symbol, side, 100, 11);
     EXPECT_CALL(venue, send_order(_,_,_,_,_)).WillOnce(Return(exch_id_4));
-    book.send_order(our_id_4, symbol, side, 98, 8);
-    book.add_order(exch_id_4, symbol, side, 98, 8);
+    book.send_order(our_id_4, symbol, side, 97, 8);
+    book.add_order(exch_id_4, symbol, side, 97, 8);
 
-    TopLevel best_ask = book.best_ask(symbol);
-    EXPECT_EQ(best_ask.price, 98);
-    EXPECT_EQ(best_ask.qty, 15);
+    TopLevel levels[4];
+    EXPECT_EQ(book.get_top_levels(symbol, side, 4, levels), 4);
 
-    TopLevel levels[2];
-    EXPECT_EQ(book.get_top_levels(symbol, side, 2, levels), 2);
-
-    EXPECT_EQ(levels[0].price, 98);
-    EXPECT_EQ(levels[0].count, 2);
-    EXPECT_EQ(levels[0].qty, 15);
-    EXPECT_EQ(levels[1].price, 99);
-    EXPECT_EQ(levels[1].count, 2);
-    EXPECT_EQ(levels[1].qty, 18);
-
+    EXPECT_EQ(levels[0].price, 97);
+    EXPECT_EQ(levels[0].count, 1);
+    EXPECT_EQ(levels[0].qty, 8);
+    EXPECT_EQ(levels[1].price, 98);
+    EXPECT_EQ(levels[1].count, 1);
+    EXPECT_EQ(levels[1].qty, 7);
+    EXPECT_EQ(levels[2].price, 99);
+    EXPECT_EQ(levels[2].count, 1);
+    EXPECT_EQ(levels[2].qty, 5);
+    EXPECT_EQ(levels[3].price, 100);
+    EXPECT_EQ(levels[3].count, 1);
+    EXPECT_EQ(levels[3].qty, 11);
 }
+
+TEST(Query, FifoOrder){
+    uint64_t our_id_1 = 12; uint64_t our_id_2 = 13; uint64_t our_id_3 = 14;
+    uint64_t exch_id_1 = 16; uint64_t exch_id_2 = 19; uint64_t exch_id_3 = 25;
+    int side = 0; uint16_t symbol = 2; uint16_t price = 10000;
+    uint64_t qty_1 = 15; uint64_t qty_2 = 11; uint64_t qty_3 = 17;
+
+    VenueMock venue;
+    MultiOrderBook book(venue);
+    
+    EXPECT_CALL(venue, send_order(_,_,_,_,_)).WillOnce(Return(exch_id_1));
+    book.send_order(our_id_1, symbol, side, price, qty_1);
+    book.add_order(exch_id_1, symbol, side, price, qty_1);
+    EXPECT_CALL(venue, send_order(_,_,_,_,_)).WillOnce(Return(exch_id_2));
+    book.send_order(our_id_2, symbol, side, price, qty_2);
+    book.add_order(exch_id_2, symbol, side, price, qty_2);
+    EXPECT_CALL(venue, send_order(_,_,_,_,_)).WillOnce(Return(exch_id_3));
+    book.send_order(our_id_3, symbol, side, price, qty_3);
+    book.add_order(exch_id_3, symbol, side, price, qty_3);
+
+    QueuePosition q1 = book.get_queue_position(our_id_1);
+    QueuePosition q2 = book.get_queue_position(our_id_2);
+    QueuePosition q3 = book.get_queue_position(our_id_3);
+
+    EXPECT_EQ(q1.index, 0);
+    EXPECT_EQ(q1.qty_ahead, 0);
+    EXPECT_EQ(q2.index, 1);
+    EXPECT_EQ(q2.qty_ahead, qty_1);
+    EXPECT_EQ(q3.index, 2);
+    EXPECT_EQ(q3.qty_ahead, qty_1 + qty_2);
+}
+
+TEST(Query, FifoOrderDiffPrice){
+    uint64_t our_id_1 = 12; uint64_t our_id_2 = 13;
+    uint64_t exch_id_1 = 16; uint64_t exch_id_2 = 19;
+    int side = 0; uint16_t symbol = 2;
+    uint64_t price_1 = 10001; uint64_t price_2 = 9992;
+    uint64_t qty_1 = 15; uint64_t qty_2 = 11; uint64_t qty_3 = 17;
+
+    VenueMock venue;
+    MultiOrderBook book(venue);
+    
+    EXPECT_CALL(venue, send_order(_,_,_,_,_)).WillOnce(Return(exch_id_1));
+    book.send_order(our_id_1, symbol, side, price_1, qty_1);
+    book.add_order(exch_id_1, symbol, side, price_1, qty_1);
+    EXPECT_CALL(venue, send_order(_,_,_,_,_)).WillOnce(Return(exch_id_2));
+    book.send_order(our_id_2, symbol, side, price_2, qty_2);
+    book.add_order(exch_id_2, symbol, side, price_2, qty_2);
+
+    QueuePosition q1 = book.get_queue_position(our_id_1);
+    QueuePosition q2 = book.get_queue_position(our_id_2);
+
+    EXPECT_EQ(q1.index, 0);
+    EXPECT_EQ(q1.qty_ahead, 0);
+    EXPECT_EQ(q2.index, 0);
+    EXPECT_EQ(q2.qty_ahead, 0);
+}
+
+TEST(Query, TopVolume){
+    uint64_t our_id_1 = 12; uint64_t our_id_2 = 13; uint64_t our_id_3 = 14;
+    uint64_t exch_id_1 = 16; uint64_t exch_id_2 = 19; uint64_t exch_id_3 = 25;
+    int side = 0; uint16_t symbol = 2;
+    uint64_t price_1 = 607; uint64_t price_2 = 599; uint64_t price_3 = 599;
+    uint64_t qty_1 = 27; uint64_t qty_2 = 2; uint64_t qty_3 = 15;
+
+    VenueMock venue;
+    MultiOrderBook book(venue);
+    
+    EXPECT_CALL(venue, send_order(_,_,_,_,_)).WillOnce(Return(exch_id_1));
+    book.send_order(our_id_1, symbol, side, price_1, qty_1);
+    book.add_order(exch_id_1, symbol, side, price_1, qty_1);
+    EXPECT_CALL(venue, send_order(_,_,_,_,_)).WillOnce(Return(exch_id_2));
+    book.send_order(our_id_2, symbol, side, price_2, qty_2);
+    book.add_order(exch_id_2, symbol, side, price_2, qty_2);
+    EXPECT_CALL(venue, send_order(_,_,_,_,_)).WillOnce(Return(exch_id_3));
+    book.send_order(our_id_3, symbol, side, price_3, qty_3);
+    book.add_order(exch_id_3, symbol, side, price_3, qty_3);
+
+    // corner case when depth = 0 actually does not give the best price
+    // its due to problem requirements [best - depth + 1, best] gives interval [best + 1, best] for bid
+    // when depth = 0 which actually exclutes the first level
+    EXPECT_EQ(book.volume_near_best(symbol, side, 0), 0);
+    EXPECT_EQ(book.volume_near_best(symbol, side, 1), qty_1);
+
+    // the formula means that the level has to be > best - depth, 
+    // 607 - 8 = 599 will not include level at 599
+    // 607 - 9 = 598 will include 599 level
+    EXPECT_EQ(book.volume_near_best(symbol, side, 8), qty_1);
+    EXPECT_EQ(book.volume_near_best(symbol, side, 9), qty_1 + qty_2 + qty_3);
+    EXPECT_EQ(book.volume_near_best(symbol, side, 13), qty_1 + qty_2 + qty_3);
+}
+
+TEST(Query, EMPTY){
+    VenueMock venue;
+    MultiOrderBook book(venue);
+    uint16_t symbol = 1;
+
+    TopLevel bid = book.best_bid(symbol);
+    EXPECT_EQ(bid.price, 0);
+    EXPECT_EQ(bid.qty, 0);
+    EXPECT_EQ(bid.count, 0);
+
+    TopLevel ask = book.best_ask(symbol);
+    EXPECT_EQ(ask.price, 0);
+    EXPECT_EQ(ask.qty, 0);
+    EXPECT_EQ(ask.count, 0);
+
+    TopLevel levels[1];
+    EXPECT_EQ(book.get_top_levels(symbol, 0, 1, levels), 0);
+    EXPECT_EQ(book.get_top_levels(symbol, 1, 1, levels), 0);
+
+    EXPECT_EQ(book.volume_near_best(symbol, 0, 0), 0);
+    EXPECT_EQ(book.volume_near_best(symbol, 1, 0), 0);
+
+    QueuePosition q = book.get_queue_position(1000);
+    EXPECT_EQ(q.index, -1);
+    EXPECT_EQ(q.qty_ahead, 0);
+}
+
+TEST(Cancel, Basic){
+ uint64_t our_id = 12;
+    uint64_t exchange_id = 16;
+    int side = 0;
+    uint16_t symbol = 2;
+    uint64_t price = 1001;
+    uint64_t quantity = 47;
+    VenueMock venue;
+    MultiOrderBook book(venue);
+    
+    EXPECT_CALL(venue, send_order(_,_,_,_,_)).WillOnce(Return(exchange_id));
+    book.send_order(our_id, symbol, side, price, quantity);
+    book.add_order(exchange_id, symbol, side, price, quantity);
+
+    TopLevel bid = book.best_bid(symbol);
+    EXPECT_EQ(bid.count, 1);
+    EXPECT_EQ(bid.qty, quantity);
+    EXPECT_EQ(bid.price, price);
+
+    book.cancel_our_order(our_id);
+    book.cancel_order(exchange_id);
+
+    bid = book.best_bid(symbol);
+    EXPECT_EQ(bid.count, 0);
+    EXPECT_EQ(bid.qty, 0);
+    EXPECT_EQ(bid.price, 0);
+}
+
+TEST(Cancel, QueueOrder){
+    uint64_t our_id_1 = 12; uint64_t our_id_2 = 13; uint64_t our_id_3 = 14; 
+    uint64_t exch_id_1 = 16; uint64_t exch_id_2 = 19; uint64_t exch_id_3 = 25;
+    int side = 1; uint16_t symbol = 2; uint16_t price = 5993;
+    uint64_t qty_1 = 15; uint64_t qty_2 = 11; uint64_t qty_3 = 17;
+
+    VenueMock venue;
+    MultiOrderBook book(venue);
+    
+    EXPECT_CALL(venue, send_order(_,_,_,_,_)).WillOnce(Return(exch_id_1));
+    book.send_order(our_id_1, symbol, side, price, qty_1);
+    book.add_order(exch_id_1, symbol, side, price, qty_1);
+    EXPECT_CALL(venue, send_order(_,_,_,_,_)).WillOnce(Return(exch_id_2));
+    book.send_order(our_id_2, symbol, side, price, qty_2);
+    book.add_order(exch_id_2, symbol, side, price, qty_2);
+    EXPECT_CALL(venue, send_order(_,_,_,_,_)).WillOnce(Return(exch_id_3));
+    book.send_order(our_id_3, symbol, side, price, qty_3);
+    book.add_order(exch_id_3, symbol, side, price, qty_3);
+
+    QueuePosition q1 = book.get_queue_position(our_id_1);
+    QueuePosition q2 = book.get_queue_position(our_id_2);
+    QueuePosition q3 = book.get_queue_position(our_id_3);
+
+    EXPECT_EQ(q1.index, 0);
+    EXPECT_EQ(q1.qty_ahead, 0);
+    EXPECT_EQ(q2.index, 1);
+    EXPECT_EQ(q2.qty_ahead, qty_1);
+    EXPECT_EQ(q3.index, 2);
+    EXPECT_EQ(q3.qty_ahead, qty_1 + qty_2);
+
+    book.cancel_our_order(our_id_2);
+    book.cancel_order(exch_id_2);
+
+    q1 = book.get_queue_position(our_id_1);
+    q2 = book.get_queue_position(our_id_2);
+    q3 = book.get_queue_position(our_id_3);
+
+    EXPECT_EQ(q1.index, 0);
+    EXPECT_EQ(q1.qty_ahead, 0);
+    EXPECT_EQ(q2.index, -1);
+    EXPECT_EQ(q2.qty_ahead, 0);
+    EXPECT_EQ(q3.index, 1);
+    EXPECT_EQ(q3.qty_ahead, qty_1);
+
+    book.cancel_our_order(our_id_1);
+    book.cancel_order(exch_id_1);
+
+    q1 = book.get_queue_position(our_id_1);
+    q2 = book.get_queue_position(our_id_2);
+    q3 = book.get_queue_position(our_id_3);
+
+    EXPECT_EQ(q1.index, -1);
+    EXPECT_EQ(q1.qty_ahead, 0);
+    EXPECT_EQ(q2.index, -1);
+    EXPECT_EQ(q2.qty_ahead, 0);
+    EXPECT_EQ(q3.index, 0);
+    EXPECT_EQ(q3.qty_ahead, 0);
+}
+
